ada077b06ed4553782b60a8d22aade97dff8249c
Thorsten Claus <thorstenclaus@web.de>
2021-01-09 07:57:32 +0100

    Selection of images from list

--- Sources/SwiftLibRaw/MetaData/MetaDataInformation.swift
+++ Sources/SwiftLibRaw/MetaData/MetaDataInformation.swift
@@ -8,13 +8,15 @@
 import Foundation
 import libraw
 
+/// Collects all metadata from raw data object
 public struct MetaDataInformation {
     public let lensinfo : Lense
     public let sizes : ImageSizes
     public let parameters : ImageParameters
     public let otherInformation : ImageOtherInformation
     
-    init(_ rawdata: UnsafeMutablePointer<libraw_data_t>) {
+    /// Creates a new object with data from already opened image
+    public init(_ rawdata: UnsafeMutablePointer<libraw_data_t>) {
         
         let data = rawdata.pointee
         
